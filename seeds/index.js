fc3ed95888408c8b0878b40f667b4f54dffa62df
George <gms1980@icloud.com>
2021-05-05 21:02:19 -0700

    updated index.js

---
+++ seeds/index.js
@@ -0,0 +1,26 @@
+const seedCategory = require('./category-seeds');
+const seedTruck = require('./truck-seeds');
+const seedUser = require('./comment-seeds');
+const seedVotes = require('./vote-seeds');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('--------------');
+  await seedCategory();
+  console.log('--------------');
+
+  await seedTruck();
+  console.log('--------------');
+
+  await seedUser();
+  console.log('--------------');
+
+  await seedVotes();
+  console.log('--------------');
+
+  process.exit(0);
+};
+
+seedAll();
