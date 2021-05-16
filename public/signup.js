5fe19b4caaa60d03cc0e0eaf6c7cb1cb51d8f27b
wbwd <77216609+kmcpherson49@users.noreply.github.com>
2021-05-16 12:01:34 -0700

    add readme

--- public/signup.js
+++ public/signup.js
@@ -1,19 +1,18 @@
 $(document).ready(function(){
     var signup = $('form.signup');
-    var email = $('input#emailInput');
-    var password = $('input#passwordInput');
-    var username = $('input#usernameInput');
 
-
-    signup.on('submit', function(event) {
+    $('signUp').click(function(event) {
         event.preventDefault();
+        console.log('clicked');
+        var email = $('input#emailInput');
+        var password = $('input#passwordInput');
+        var username = $('input#usernameInput');
+
         var userInput = {
-            email: email.val().trim(),
-            password: password.val().trim(),
-            username: username.val().trim()
+            email: email.val(),
+            password: password.val(),
+            username: username.val()
         };
-
-
 
         if (!userInput.email || !userInput.password || !userInput.username) {
             return;
